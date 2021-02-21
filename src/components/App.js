3338a5fc4f9cb9a074dd7f4e120491ca2820d500
Bartosz Pniewski <pniewski.b@o2.pl>
2021-02-21 21:39:00 +0100

    Refactoring the App Removing a Callback

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-  componentDidMount() {
-    this.onTermSubmit('cars')
-  }
+  useEffect(() => {
+    onTermSubmit('cars');
+  }, []);
 
-  onTermSubmit = async (term) => {
+  const onTermSubmit = async (term) => {
     const response = await youtube.get('/search',{
       params: {
         q: term
       }
     });
 
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0]
-    });
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
   };
 
-  onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video });
-  }
 
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="nine wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="seven wide column">
-              <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
-            </div>
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={onTermSubmit} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="nine wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="seven wide column">
+            <VideoList
+            onVideoSelect={setSelectedVideo}
+            videos={videos}/>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
